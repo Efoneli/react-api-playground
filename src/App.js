44b19da9a5d690bf15e131374c3afa4294e595dc
Efoneli <abbaefoneli@gmail.com>
2023-02-26 13:21:00 +0100

    create react app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import Header from './components/Header/Header';
+import List from './components/List/List';import Map from './components/Map/Map';
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%'}}>
+        <List />
+      </Grid>
+      <Grid item xs={12} md={8}>
+        <Map />
+      </Grid>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Autocomplete } from '@react-google-maps/api'; 
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/core'
+
+import useStyles from './style';
+
+const Header = () => {
+  return (
+    <AppBar position='static'>
+        
+    </AppBar>
+  )
+}
+
+export default Header
